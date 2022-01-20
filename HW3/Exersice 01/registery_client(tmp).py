ec33a5635957119eaf4be09261cc9e157e41ebd1
Sepehr-A <sepehr.alidousti@gmail.com>
2022-01-20 11:37:00 +0100

    brief changes

--- HW3/Exersice 01/registery_client(tmp).py
+++ HW3/Exersice 01/registery_client(tmp).py
@@ -1,11 +1,22 @@
 import base64
+import json
 
-url = 'http://0.0.0.0:8080'
+import requests
+
+url = 'http://127.0.0.1:8080'
 url_add = url + '/add'
-model_name = '../Prerequisite/kws_dscnn_True.tflite'
+model_name = 'CNN.tflite'
 
 with open(model_name, 'rb') as fp:
     model_string = fp.read()
     model_base64 = base64.b64encode(model_string)
 
 body_dict = {'model': model_base64, 'name': 'cnn.tflite'}
+
+json_body=json.dumps(list(body_dict))
+r = requests.post(url_add, json=json_body)
+if r.status_code == 200:
+    body = r.json()
+    print(body)
+else:
+    print('Error:', r.status_code)
