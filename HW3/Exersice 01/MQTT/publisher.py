63b5d424c29a54fd47b88ccb20d04434f8d77d9e
Sepehr-A <sepehr.alidousti@gmail.com>
2022-01-22 13:06:23 +0100

    adding MQTT

---
+++ HW3/Exersice 01/MQTT/publisher.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from MQTT.DoSomething import DoSomething
+
+
+def Alert(type_alert, predicted, actual):
+    now = datetime.now()
+    dt_string = now.strftime("(%d/%m/%Y %H:%M:%S)")
+    msg = {'datTime': dt_string, 'Quantity': type_alert, 'Predicted': predicted, 'Actual': actual}
+    publisher = DoSomething("publisher 1")
+    publisher.run()
+    publisher.myMqttClient.myPublish('', msg)
+    publisher.end()
---
+++ HW3/Exersice 01/MQTT/subscriber.py
@@ -0,0 +1,9 @@
+from MQTT.DoSomething import DoSomething
+import time
+
+
+def get_Alert():
+    test = DoSomething("subscriber 1")
+    test.run()
+    test.myMqttClient.mySubscribe('')
+    test.end()
